c013172a9a7870c5ec30a7e14624d5f90968dd0c
Felipe <fsiqesp2@gmail.com>
2025-11-24 10:37:44 +0000

    Update emf.c
    Co-Authored-By: Radn98 <66326324+Radn98@users.noreply.github.com>

--- src/emf.c
+++ src/emf.c
@@ -517,7 +517,7 @@ void emf_move_window( t_emf *emf ){
 	{ 
 		float3* const restrict E = emf -> E; 
 		float3* const restrict B = emf -> B; 
-		int nTot = emf->nx + emf->gc[1] - (-emf->gc[0]);
+		int nTot = 1 + emf->nx + emf->gc[1] - (-emf->gc[0]);
         float3 *restrict ECopy = malloc(nTot * sizeof(float3));
         float3 *restrict BCopy = malloc(nTot * sizeof(float3));
 		const float3 zero_fld = {0.,0.,0.}; 
