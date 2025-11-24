278cb0eb93fbf5806f096d161d3f537d0ee5bbd5
Felipe <fsiqesp2@gmail.com>
2025-11-24 15:17:39 +0000

    Update current.c

--- src/current.c
+++ src/current.c
@@ -297,20 +297,18 @@ void kernel_x( t_current* const current, const float sa, const float sb ){
             J[i] = fs;
             
         }
+    }
         
         
         // Update x boundaries for periodic boundaries
         if ( current -> bc_type == CURRENT_BC_PERIODIC ) {
-            #pragma omp for
             for(int i = -current->gc[0]; i<0; i++){
                 J[ i ] = J[ current->nx + i ];
             }
-            
-            #pragma omp for
+
             for (int i=0; i<current->gc[1]; i++){
                 J[ current->nx + i ] = J[ i ];
             }
-        }
     }
     
     free(JCopy);
